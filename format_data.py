6c708daab9c78bc3f93a1b740949731fd17dffeb
zman572 <uzachpc@gmail.com>
2025-07-25 15:07:04 -0400

    Cleaned up code

--- format_data.py
+++ format_data.py
@@ -10,24 +10,6 @@ df = df[["Season", "Short Conference Name", "Adjusted Tempo",
         "Full Team Name", "Seed", "Region", "Post-Season Tournament"]]
 
 
-
-#Set teams that were in march madness to 1, otherwise set to 0.
-
-'''df = df[df["Season"] == 2025]
-df["Efficiency_Ratio"] = df["Adjusted Offensive Efficiency"] / df["Adjusted Defensive Efficiency"]
-
-# Define major conferences (this can be controversial lol)
-big_conferences = ["ACC", "SEC", "B12"]
-median_eff_ratio = df["Efficiency_Ratio"].median()
-
-def compute_upset_propensity(row):
-    if row["Short Conference Name"] not in big_conferences and row["Efficiency_Ratio"] > median_eff_ratio:
-        return 1
-    else:
-        return 0
-
-df["Upset_Propensity"] = df.apply(compute_upset_propensity, axis=1)'''
-
 df["Tournament Target"] = (df["Post-Season Tournament"] == "March Madness").astype(int)
 
 features = ["Adjusted Offensive Efficiency", "Adjusted Defensive Efficiency",
